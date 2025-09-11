c98056fe84e9bfffa7b4bcb8fa72342d637fd8fa
MessiasSj <evandromessias.sj@gmail.com>
2025-09-11 20:53:14 +0000

    Salvamento parcial 1

---
+++ desafio_sup_trunfo_nvl_aventureiro.c
@@ -0,0 +1,14 @@
+/* Neste primeiro nível o Objetivo é construir a base do nosso jogo 
+Super Trunfo de Países: um sistema para cadastrar as cartas com informações sobre as cidades. */
+
+#include <stdio.h>
+// inicio da função
+int main(){
+    
+
+
+
+
+
+
+}
